aa71a53e98287914f34190ef5ee1f42e65691fc0
Alpimax <alperrozkann@gmail.com>
2022-09-08 22:44:03 -0400

    class completed.

--- src/Game/Ticket.java
+++ src/Game/Ticket.java
@@ -1,8 +1,12 @@
 package Game;
 public class Ticket {
+
+
     public static void main(String[] args) {
 //class created
         // class completed
-
+        for (int i = 0; i < 20; i++) {
+            System.out.println(i);
+        }
     }
 }
