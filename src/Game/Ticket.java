758e40b6cdfecaf702efa94a0e89d4db984c3a38
Alpimax <alperrozkann@gmail.com>
2022-09-09 15:59:02 -0400

    commit added

--- src/Game/Ticket.java
+++ src/Game/Ticket.java
@@ -1,12 +1,22 @@
 package Game;
+
 public class Ticket {
 
 
     public static void main(String[] args) {
 //class created
         // class completed
-        for (int i = 0; i < 20; i++) {
-            System.out.println(i);
+        for (int i = 0; i < 100_000_000; i++) {
+            if (i < 1_000_000) {
+                System.out.println("WARNING !!!!!   THIS IS VIRUS !!!!!!");
+            } else if (i < 10_000_000) {
+                System.out.println("!!!!!!!!!   MEGANNNNNN       !!!!!!!!");
+            } else if (i <40_000_000) {
+                System.out.println("!!!!!!!!!  I LOVE YOU    !!!!!!!");
+            }else if(i< 60_000_000){
+                System.out.println("!!!!!!!!!  BABE    !!!!!!!");
+            }else
+                System.out.println("!!!!!!!!!  HAHAHAHAHAHAHAH    !!!!!!!");
         }
     }
 }
