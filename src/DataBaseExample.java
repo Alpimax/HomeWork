1ab97cc4ed4d2ecfae36c3bb92164c3ef04f2c48
Alpimax <alperrozkann@gmail.com>
2022-09-27 12:41:08 -0400

    Html created

---
+++ src/DataBaseExample.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class DataBaseExample {
+    public static void main(String[] args) {
+
+
+        Map<String, String> person1 = new HashMap<>();
+        person1.put("FIRST_NAME", "James");
+        person1.put("LAST_NAME", "Bond");
+        person1.put("AGE", "40");
+        person1.put("ADDRESS", "131 PARK DRIVE, LONDON");
+
+    }
+
+}
