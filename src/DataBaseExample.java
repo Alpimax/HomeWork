ee43e39a67bf30e577f528a1c64fc682bf53e38d
Alpimax <alperrozkann@gmail.com>
2022-10-01 08:27:46 -0400

    work on it created

--- src/DataBaseExample.java
+++ src/DataBaseExample.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,6 +12,27 @@ public class DataBaseExample {
         person1.put("AGE", "40");
         person1.put("ADDRESS", "131 PARK DRIVE, LONDON");
 
+        firstSwap(new String[]{"ax", "bx", "ay", "by", "ai", "aj", "bx", "by"});
     }
+    public static String[] firstSwap(String[] strings) {
+        Map<String,Integer> map = new HashMap<>();
+
+        for(int i =0;i<strings.length;i++){
+            String letter =strings[i].substring(0,1);
+            if(!map.containsKey(letter)){
+                map.put(letter,i);
+            }else{
+                int first = map.get(letter);
+                String save = strings[first];
+                strings[first] = strings[i];
+                strings[i] = save;
+                map.remove(letter);
+
+            }
+        }
+        System.out.println(Arrays.toString(strings));
+        return strings;
+    }
+
 
 }
