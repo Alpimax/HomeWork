03daf094660041edf3047264b0679a5c9c0c9686
Alpimax <alperrozkann@gmail.com>
2022-10-03 10:36:39 -0400

    work on it created

---
+++ src/SquareProject/ConfigReader.java
@@ -0,0 +1,16 @@
+package SquareProject;
+
+import java.util.Properties;
+
+public class ConfigReader {
+
+    private static Properties properties = new Properties();
+    /*
+        private: only accessible in the same class
+        static: access by the class name instead of needed to create an object
+        Properties: we are creating an object of this class to connect a properties (test.properties) to a java object
+     */
+
+
+
+}
