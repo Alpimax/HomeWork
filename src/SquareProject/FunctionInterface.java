f9380026f14d438c59ea4c4f02d4d24c4a86ca3a
Alpimax <alperrozkann@gmail.com>
2022-10-04 10:21:06 -0400

    FunctionInterface  created

--- src/SquareProject/FunctionInterface.java
+++ src/SquareProject/FunctionInterface.java
@@ -1,4 +1,13 @@
 package SquareProject;
 
 public class FunctionInterface {
+           /*
+        Use Predicate Functional interface to:
+            1.1 Create a function that can verify if a number contains duplicate digits
+         */
+
+    //Consumer: 1.2 Create a function that prints the middle character/s of a String
+
+
+    //Function: 1.1 Create a function that can return the sum of digits from a string that contains number digits
 }
